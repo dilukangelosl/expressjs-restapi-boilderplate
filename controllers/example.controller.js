9d08466d5bee6f3ad2759b074f2243b12b44c438
Diluk angelo <dilukangelo@gmail.com>
2020-09-23 09:30:14 +0530

    small fix

--- controllers/example.controller.js
+++ controllers/example.controller.js
@@ -4,42 +4,45 @@ const constants = require('../constants');
 module.exports = {
 
     findAll : async (req,res,next) => {
+      let response = constants.defaultResponse;
       try {
         let data =  await exampleService.findAllUsers();
         
-        let response = constants.defaultResponse;
+       
         response.statusCode = 200 ;
         response.status = constants.status.SUCCESS;
         response.data = data;
         response.message = constants.exampleServiceMessages.CREATED ; 
-        return res.status(response.statusCode).send(response);
+       
       } catch (error) {
         response.statusCode = 401 ;
         response.status = constants.status.FAILED;
         response.data = error;
         response.message = constants.exampleServiceMessages.CREATE_ERROR ; 
-        return res.status(response.statusCode).send(response);
+       
       }
-        
+      return res.status(response.statusCode).send(response);
     },
 
     add : async (req,res,next) => {
+      let response = constants.defaultResponse;
         try {
             let data =  await exampleService.adduser({...req.body});
             
-            let response = constants.defaultResponse;
+           
             response.statusCode = 200 ;
             response.status = constants.status.SUCCESS;
             response.data = data;
             response.message = constants.exampleServiceMessages.CREATED ; 
-            return res.status(response.statusCode).send(response);
+            
           } catch (error) {
             response.statusCode = 401 ;
             response.status = constants.status.FAILED;
             response.data = error;
             response.message = constants.exampleServiceMessages.CREATE_ERROR ; 
-            return res.status(response.statusCode).send(response);
+            
           }
+          return res.status(response.statusCode).send(response);
     },
 
     update : async (req,res,next) => {
